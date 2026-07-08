bed783fbf1fdf83252a7993f42bff0ddab99a93c
Raikaru <raikarualt001@gmail.com>
2026-07-07 21:31:44 -0400

    Match wave eighteen time limit function

--- src/Yajima/y_timeLimit.c
+++ src/Yajima/y_timeLimit.c
@@ -1,7 +1,30 @@
 #include "Yajima/y_timeLimit.h"
 
+typedef struct YTimeLimitInner
+{
+    u8 pad0[4];
+    u8 mode;
+    u8 pad5[0x3f];
+    s16 field44;
+} YTimeLimitInner;
+
+typedef struct YTimeLimitWork
+{
+    u8 pad0[0x3c];
+    YTimeLimitInner* inner;
+} YTimeLimitWork;
+
+extern YTimeLimitWork* iGpffffba08;
+
 // FUN_0045a400
 void Y_TimeLimit_0045a400()
 {
-    // TODO
+    YTimeLimitInner* inner;
+
+    if (iGpffffba08 != NULL)
+    {
+        inner = iGpffffba08->inner;
+        inner->field44 = 0;
+        inner->mode = 6;
+    }
 }
