c1d4139fe12bc076516190643dbbf55a9d698f59
Raikaru <raikarualt001@gmail.com>
2026-07-08 23:51:07 -0400

    Add sfl_cursor.c, sfl_count.c; bp_dialog.c +b678 init/cleanup

--- src/Main/Battle/Cmd/bp_dialog.c
+++ src/Main/Battle/Cmd/bp_dialog.c
@@ -16,3 +16,22 @@ u32 bpDialog0025c1e0(void)
     K_ASSERT(sBpDialog368 != NULL, 0x3c);
     return sBpDialog368[4];
 }
+
+void FUN_003b0170();
+
+// FUN_0025bdf0
+void bpDialog0025bdf0(u32* param_1)
+{
+    *param_1 = 0;
+    sBpDialog368 = param_1;
+}
+
+// FUN_0025be00
+void bpDialog0025be00(void)
+{
+    K_ASSERT(sBpDialog368 != NULL, 0x3c);
+    if (*sBpDialog368 & 1) {
+        FUN_003b0170(sBpDialog368[1]);
+    }
+    sBpDialog368 = NULL;
+}
