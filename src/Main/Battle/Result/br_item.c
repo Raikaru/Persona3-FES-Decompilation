67e14e7b488483d86641fc50e91a70d09c2a42a7
Raikaru <raikarualt001@gmail.com>
2026-07-09 04:23:15 -0400

    br_item.c: +2 (25d8a0 init, 25f510 cleanup)

--- src/Main/Battle/Result/br_item.c
+++ src/Main/Battle/Result/br_item.c
@@ -1,7 +1,13 @@
 #include "Kernel/Kwln/kwlnTask.h"
 #include "Kosaka/k_assert.h"
+#include "rw/rwplcore.h"
 
 static u32* sBrItem; // DAT_007ce370
+static u32 sBrItem374; // DAT_007ce374
+void FUN_0025f5d0();
+float FUN_0021ea00();
+void FUN_0021eac0(void*, float);
+void FUN_0025d9e0();
 
 void FUN_0025da30();
 void FUN_0025ed40();
@@ -40,4 +46,46 @@ u32 brItem0025f4a0(void)
         return -1;
     }
     return 0;
+}
+
+// FUN_0025d8a0
+void brItem0025d8a0(void* param_1)
+{
+    u32* puVar1;
+    float fVar2;
+    float fVar3;
+
+    K_ASSERT(sBrItem != NULL, 0x75);
+    puVar1 = sBrItem;
+    sBrItem[2] = (u32)param_1;
+    FUN_0025f5d0(puVar1 + 0x104, param_1, 0x34);
+    fVar2 = FUN_0021ea00(0x1e);
+    FUN_0021eac0(puVar1 + 0x104, fVar2);
+    FUN_0025f5d0(puVar1 + 0x144, puVar1[2], 0x35);
+    fVar2 = FUN_0021ea00(0x1e);
+    FUN_0021eac0(puVar1 + 0x144, fVar2);
+    FUN_0025f5d0(puVar1 + 0x184, puVar1[2], 0x36);
+    fVar2 = FUN_0021ea00(0x1e);
+    FUN_0021eac0(puVar1 + 0x184, fVar2);
+    FUN_0025f5d0(puVar1 + 0x1c4, puVar1[2], 0x3a);
+    fVar2 = FUN_0021ea00(0x32);
+    FUN_0021eac0(puVar1 + 0x1c4, fVar2);
+    fVar3 = FUN_0021ea00(0x32);
+    ((float*)puVar1)[0x206] = fVar3;
+    ((float*)puVar1)[0x216] = fVar3;
+    ((float*)puVar1)[0x226] = fVar3;
+    FUN_0025d9e0();
+    *puVar1 |= 1;
+}
+
+// FUN_0025f510
+void brItem0025f510(int param_1)
+{
+    u32 uVar1;
+
+    uVar1 = *(u32*)(param_1 + 0x3c);
+    K_ASSERT(sBrItem != NULL, 0x75);
+    sBrItem = NULL;
+    RwFree((void*)uVar1);
+    sBrItem374 = 0;
 }
