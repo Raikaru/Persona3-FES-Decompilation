236f88758ca7d8f857ebeaa605b4c8c8ab0ea371
Raikaru <raikarualt001@gmail.com>
2026-07-08 23:18:14 -0400

    Match 4 more brp_res getters (dual work globals)

--- src/Main/Battle/Result/brp_res.c
+++ src/Main/Battle/Result/brp_res.c
@@ -10,3 +10,37 @@ void brpRes0024ba30(u32* param_1)
     param_1[1] = 0;
     sBrpResWork = param_1;
 }
+
+static u32* sBrpRes334; // 007ce334
+
+// FUN_0024bed0
+u32 brpRes0024bed0(void)
+{
+    K_ASSERT(sBrpResWork != NULL, 0x39);
+    return *sBrpResWork & 1;
+}
+
+// FUN_0024bf20
+u32 brpRes0024bf20(s32 param_1)
+{
+    K_ASSERT(sBrpResWork != NULL, 0x39);
+    return sBrpResWork[param_1 + 4];
+}
+
+// FUN_0024c040
+u32 brpRes0024c040(void)
+{
+    K_ASSERT(sBrpRes334 != NULL, 0x39);
+    return *sBrpRes334 & 2;
+}
+
+// FUN_0024c090
+u32 brpRes0024c090(void)
+{
+    u32* work;
+
+    K_ASSERT(sBrpRes334 != NULL, 0x39);
+    work = sBrpRes334;
+    K_ASSERT(work[1] & 2, 0xce);
+    return work[8];
+}
