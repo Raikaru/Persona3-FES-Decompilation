c0697e50f345ef342ad4d925ae7a9118d3ec5570
Raikaru <raikarualt001@gmail.com>
2026-07-09 03:18:18 -0400

    h_camp.c: add H_CampRootDraw create/destroy/getter/setters (6)

--- src/Camp/h_camp.c
+++ src/Camp/h_camp.c
@@ -77,3 +77,74 @@ KwlnTask* h_camp0011a770(KwlnTask* parent)
     }
     return task;
 }
+
+void* FUN_0011b420(KwlnTask*);
+void FUN_0011d3a0(float, u32*);
+void FUN_0011cc80(float, u32*);
+
+// FUN_0011baa0
+void h_camp0011baa0(int param_1)
+{
+    RwFree(*(void**)(param_1 + 0x3c));
+    uGpffffb26c = 0;
+}
+
+// FUN_0011b9a0
+u32 h_camp0011b9a0(int param_1)
+{
+    return *(u32*)(*(int*)(param_1 + 0x3c) + 0xc);
+}
+
+// FUN_0011b9b0
+void h_camp0011b9b0(int param_1)
+{
+    u32* workData;
+
+    workData = *(u32**)(param_1 + 0x3c);
+    FUN_0011d3a0(100.0f, workData);
+    workData[0] = 7;
+    workData[1] = 0x16;
+}
+
+// FUN_0011ba00
+void h_camp0011ba00(int param_1)
+{
+    u32* workData;
+
+    workData = *(u32**)(param_1 + 0x3c);
+    FUN_0011d3a0(100.0f, workData);
+    workData[0] = 8;
+    workData[1] = 0x16;
+}
+
+// FUN_0011ba50
+void h_camp0011ba50(int param_1)
+{
+    u32* workData;
+
+    workData = *(u32**)(param_1 + 0x3c);
+    FUN_0011cc80(100.0f, workData);
+    workData[0] = 5;
+    workData[1] = 0;
+    workData[3] = 0xffffffff;
+}
+
+// FUN_0011bad0
+KwlnTask* h_camp0011bad0(KwlnTask* parent, u32 param_2, u32 param_3)
+{
+    u32* workData;
+    KwlnTask* task;
+
+    workData = (u32*)RwCalloc(1, 0x2c, 0x40000);
+    if (workData == NULL) {
+        return NULL;
+    }
+    task = kwlnTaskCreate(parent, "H_CampRootDraw", 0x18bf, FUN_0011b420, (KwlnTaskDestroyFunc)h_camp0011baa0, workData);
+    if (task == NULL) {
+        return NULL;
+    }
+    workData[9] = param_2;
+    workData[3] = 0xffffffff;
+    workData[2] = param_3;
+    return task;
+}
