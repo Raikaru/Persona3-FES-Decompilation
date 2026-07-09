7f2212c0726805956bb2edf0bf13597c311095b1
Raikaru <raikarualt001@gmail.com>
2026-07-08 21:40:55 -0400

    Match K_Cmd_001c2500/K_Cmd_001c28d0

--- src/Kosaka/k_command.c
+++ src/Kosaka/k_command.c
@@ -32,6 +32,16 @@ u32 K_Cmd_SKIP_TO_DARK_HOUR()
     return true;
 }
 
+void FUN_001a0150();
+
+// FUN_001c2500
+u32 K_Cmd_001c2500()
+{
+    FUN_001a0150((u16)scrGetIntPara(0), scrGetIntPara(1));
+
+    return true;
+}
+
 // FUN_001c2550
 u32 K_Cmd_REQ_CLND_SKIP()
 {
@@ -146,6 +156,16 @@ u32 K_Cmd_DATE_IN_RANGE()
     {
         scrSetIntReturnVal(0);
     }
+
+    return true;
+}
+
+void FUN_00103cb0();
+
+// FUN_001c28d0
+u32 K_Cmd_001c28d0()
+{
+    FUN_00103cb0();
 
     return true;
 }
