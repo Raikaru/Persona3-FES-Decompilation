c3f656b383c10575ed3cd2e7c6943a8383985220
Raikaru <raikarualt001@gmail.com>
2026-07-08 23:59:32 -0400

    Add sfl_psel.c (5 accessors), br_item.c (3 fns)

--- src/Main/Battle/Result/br_item.c
+++ src/Main/Battle/Result/br_item.c
@@ -1,3 +1,43 @@
-#include "Main/Battle/Result/br_item.h"
+#include "Kernel/Kwln/kwlnTask.h"
+#include "Kosaka/k_assert.h"
 
-// TODO
+static u32* sBrItem; // DAT_007ce370
+
+void FUN_0025da30();
+void FUN_0025ed40();
+
+// FUN_0025d9e0
+void brItem0025d9e0(void)
+{
+    u32* work;
+
+    K_ASSERT(sBrItem != NULL, 0x75);
+    work = sBrItem;
+    work[3] = 0;
+    work[1] = 0;
+    *work |= 2;
+}
+
+// FUN_0025f570
+void brItem0025f570(void)
+{
+    u32* work;
+
+    K_ASSERT(sBrItem != NULL, 0x75);
+    work = sBrItem;
+    work[3] = 0;
+    work[1] = 1;
+    *work |= 2;
+}
+
+// FUN_0025f4a0
+u32 brItem0025f4a0(void)
+{
+    FUN_0025da30();
+    FUN_0025ed40();
+    K_ASSERT(sBrItem != NULL, 0x75);
+    if ((*sBrItem & 1) == 0) {
+        return -1;
+    }
+    return 0;
+}
