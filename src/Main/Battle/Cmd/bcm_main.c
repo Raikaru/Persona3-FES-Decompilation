6d555de754de53b0b864d3593c1c08015150ccc9
Raikaru <raikarualt001@gmail.com>
2026-07-08 22:37:40 -0400

    Match bcm00207930/bcm0020ac80

--- src/Main/Battle/Cmd/bcm_main.c
+++ src/Main/Battle/Cmd/bcm_main.c
@@ -16,3 +16,16 @@ void bcm00207bf0(void)
     K_ASSERT(sBcmWork != NULL, 0x164);
     *(u32*)(sBcmWork + 0x77a0) &= ~1;
 }
+
+s32 FUN_0017d2e0();
+
+// FUN_00207930
+u32 bcm00207930(u16 param_1)
+{
+    return FUN_0017d2e0(param_1) != 0xc;
+}
+
+// FUN_0020ac80
+void bcm0020ac80(void)
+{
+}
