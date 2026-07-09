5fd0224ea8638cd7f7d664e2ef22596fce072267
Raikaru <raikarualt001@gmail.com>
2026-07-09 04:02:03 -0400

    sfl_res.c: +2 (20e8f0, 20eb40 flag-set loaders)

--- src/Main/Social/sfl_res.c
+++ src/Main/Social/sfl_res.c
@@ -3,6 +3,9 @@
 #include "rw/rwplcore.h"
 
 static u32* sSflRes; // DAT_007ce2f0
+extern char DAT_0068e1c0[];
+extern char DAT_0068e1e0[];
+u32 FUN_00100d80();
 
 // FUN_0020e9b0
 u32 sflRes0020e9b0(void)
@@ -56,4 +59,30 @@ void sflRes0020ecc0(void)
     K_ASSERT(*(u32*)(base + 4) & 0x10, 0x232);
     FUN_0021cc20(*(void**)(base + 0x80));
     *(u32*)(base + 4) &= 0xffffffef;
+}
+
+// FUN_0020e8f0
+void sflRes0020e8f0(void)
+{
+    u32* puVar1;
+
+    K_ASSERT(sSflRes != NULL, 0x65);
+    puVar1 = sSflRes;
+    K_ASSERT((~puVar1[1] & 8) != 0, 0x1f2);
+    K_ASSERT((~*puVar1 & 8) != 0, 0x1f3);
+    puVar1[0x1c] = FUN_00100d80(DAT_0068e1c0, 1);
+    *puVar1 |= 8;
+}
+
+// FUN_0020eb40
+void sflRes0020eb40(void)
+{
+    u32* puVar1;
+
+    K_ASSERT(sSflRes != NULL, 0x65);
+    puVar1 = sSflRes;
+    K_ASSERT((~*puVar1 & 0x10) != 0, 0x218);
+    K_ASSERT((~puVar1[1] & 0x10) != 0, 0x219);
+    puVar1[0x1d] = FUN_00100d80(DAT_0068e1e0, 0);
+    *puVar1 |= 0x10;
 }
