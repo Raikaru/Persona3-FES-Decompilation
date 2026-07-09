f9b28b52a33d58489243fa42a659bb02955e9a10
Raikaru <raikarualt001@gmail.com>
2026-07-08 23:15:22 -0400

    Add bp_tuta.c + brp_res.c init accessors

---
+++ src/Main/Battle/Cmd/bp_tuta.c
@@ -0,0 +1,11 @@
+#include "Kernel/Kwln/kwlnTask.h"
+#include "Kosaka/k_assert.h"
+
+static u32* sBpTutaWork; // puGpffffb658
+
+// FUN_00251a70
+void bpTuta00251a70(u32* param_1)
+{
+    *param_1 = 0;
+    sBpTutaWork = param_1;
+}
