1cd293b718080c69d1d5a7b8d4cd29ad77e876e4
Raikaru <raikarualt001@gmail.com>
2026-07-09 06:30:25 -0400

    datScript.c: +1 (311250) - completes file, contiguous/build-eligible

--- src/Main/Battle/Data/datScript.c
+++ src/Main/Battle/Data/datScript.c
@@ -17,3 +17,13 @@ u32 datScript003111f0(void)
     K_ASSERT(DAT_00957190[0] != NULL, 0x205);
     return !(*DAT_00957190[0] & 4);
 }
+
+extern u16 DAT_0095719c[]; // 0x95719c
+static u8* sUnk_007ce3f8;  // iGpffffb708
+
+// FUN_00311250
+u32 datScript00311250(void)
+{
+    K_ASSERT(DAT_0095719c[0] < 0x1d0, 0x222);
+    return (sUnk_007ce3f8[DAT_0095719c[0] * 0x2c] & 2) != 0;
+}
