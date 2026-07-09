f473d7285e1ac10d5a40115478b012593d9c28da
Raikaru <raikarualt001@gmail.com>
2026-07-09 04:40:17 -0400

    sfl_psel.c: +1 init (260a50)

--- src/Main/Social/sfl_psel.c
+++ src/Main/Social/sfl_psel.c
@@ -2,6 +2,13 @@
 #include "Kosaka/k_assert.h"
 
 static u32* sSflPsel; // puGpffffb688
+u32 FUN_0020ec50();
+u32 FUN_0021cca0();
+void FUN_0021d3b0();
+float FUN_0021ea00();
+void FUN_0021eac0(void*, float);
+void FUN_00260c20();
+void FUN_00261480();
 
 // FUN_00260a10
 void sflPsel00260a10(void)
@@ -46,4 +53,41 @@ u32 sflPsel00260cd0(void)
 {
     K_ASSERT(sSflPsel != NULL, 0x57);
     return *sSflPsel & 2;
+}
+
+// FUN_00260a50
+void sflPsel00260a50(void)
+{
+    u32* puVar1;
+    u32 uVar2;
+    u32 uVar3;
+    float fVar4;
+    float fVar5;
+
+    K_ASSERT(sSflPsel != NULL, 0x57);
+    puVar1 = sSflPsel;
+    uVar2 = FUN_0020ec50();
+    uVar3 = FUN_0021cca0(uVar2, 0x34);
+    FUN_0021d3b0(puVar1 + 0x104, uVar3);
+    fVar4 = FUN_0021ea00(0x1e);
+    FUN_0021eac0(puVar1 + 0x104, fVar4);
+    uVar3 = FUN_0021cca0(uVar2, 0x35);
+    FUN_0021d3b0(puVar1 + 0x144, uVar3);
+    fVar4 = FUN_0021ea00(0x1e);
+    FUN_0021eac0(puVar1 + 0x144, fVar4);
+    uVar3 = FUN_0021cca0(uVar2, 0x36);
+    FUN_0021d3b0(puVar1 + 0x184, uVar3);
+    fVar4 = FUN_0021ea00(0x1e);
+    FUN_0021eac0(puVar1 + 0x184, fVar4);
+    uVar3 = FUN_0021cca0(uVar2, 0x3a);
+    FUN_0021d3b0(puVar1 + 0x1c4, uVar3);
+    fVar4 = FUN_0021ea00(0x32);
+    FUN_0021eac0(puVar1 + 0x1c4, fVar4);
+    fVar5 = FUN_0021ea00(0x32);
+    ((float*)puVar1)[0x206] = fVar5;
+    ((float*)puVar1)[0x216] = fVar5;
+    ((float*)puVar1)[0x226] = fVar5;
+    FUN_00260c20();
+    FUN_00261480();
+    *puVar1 |= 1;
 }
