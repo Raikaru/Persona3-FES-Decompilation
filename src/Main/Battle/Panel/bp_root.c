1bf01790afa6532af9fdc037c11a228f11a4a269
Raikaru <raikarualt001@gmail.com>
2026-07-08 22:13:09 -0400

    Match 8 bpRoot single-call wrappers

--- src/Main/Battle/Panel/bp_root.c
+++ src/Main/Battle/Panel/bp_root.c
@@ -23,6 +23,70 @@ void bpRootDestroy(KwlnTask* btlPanelTask)
     // TODO
 }
 
+void FUN_0020f3b0();
+
+// FUN_001fdd20
+void bpRoot001fdd20()
+{
+    FUN_0020f3b0();
+}
+
+void FUN_0020f570();
+
+// FUN_001fdd40
+void bpRoot001fdd40()
+{
+    FUN_0020f570();
+}
+
+void FUN_00244120();
+
+// FUN_001fe7f0
+void bpRoot001fe7f0()
+{
+    FUN_00244120();
+}
+
+void FUN_00244a40();
+
+// FUN_001fe970
+void bpRoot001fe970()
+{
+    FUN_00244a40();
+}
+
+void FUN_00249180();
+
+// FUN_001fea70
+void bpRoot001fea70()
+{
+    FUN_00249180();
+}
+
+void FUN_0021bb50();
+
+// FUN_001fec30
+void bpRoot001fec30()
+{
+    FUN_0021bb50();
+}
+
+void FUN_0024d0c0();
+
+// FUN_001fecc0
+void bpRoot001fecc0()
+{
+    FUN_0024d0c0();
+}
+
+void FUN_00207bf0();
+
+// FUN_001fed00
+void bpRoot001fed00()
+{
+    FUN_00207bf0();
+}
+
 // FUN_001ff350
 void bpRoot001ff350()
 {
