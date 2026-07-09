05bb355af9128d88d02b1758c64a3b43912fb85a
Raikaru <raikarualt001@gmail.com>
2026-07-09 03:13:26 -0400

    h_camp.c: add H_CampDraw destroy task (11a710)

--- src/Camp/h_camp.c
+++ src/Camp/h_camp.c
@@ -1,4 +1,5 @@
 #include "Camp/h_camp.h"
+#include "rw/rwplcore.h"
 
 
 // TODO
@@ -34,4 +35,25 @@ const char* h_camp_getCharmLevelString(s16 idx)
 const char* h_camp_getCourageLevelString(s16 idx)
 {
     return courageLevelString[idx];
+}
+
+void FUN_0016f1f0();
+
+static u32 uGpffffb260;
+static u32 uGpffffb264;
+static u32 uGpffffb268;
+static u32 uGpffffb26c;
+
+// FUN_0011a710
+void h_camp0011a710(int param_1)
+{
+    void* workData;
+
+    workData = *(void**)(param_1 + 0x3c);
+    uGpffffb264 = 0;
+    uGpffffb268 = 0;
+    uGpffffb260 = 0;
+    uGpffffb26c = 0;
+    FUN_0016f1f0(0x1407, 0);
+    RwFree(workData);
 }
