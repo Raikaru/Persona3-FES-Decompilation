353cbf3f9ce317cca6d812715ffe638122158b49
Raikaru <raikarualt001@gmail.com>
2026-07-08 23:37:15 -0400

    sfl_card.c: +6 global accessors (10 total)

--- src/Main/Social/sfl_card.c
+++ src/Main/Social/sfl_card.c
@@ -1,4 +1,8 @@
 #include "Kernel/Kwln/kwlnTask.h"
+#include "Kosaka/k_assert.h"
+
+static u32* sSflCardB664; // puGpffffb664
+static u32* sSflCard354;  // DAT_007ce354
 
 // FUN_00255170
 void sflCard00255170(u32* param_1)
@@ -23,4 +27,58 @@ u32 sflCard002561c0(u32* param_1)
 u32 sflCard002561d0(u32* param_1)
 {
     return *param_1 & 0x80;
+}
+
+// FUN_00259250
+void sflCard00259250(void)
+{
+    u32* work;
+
+    K_ASSERT(sSflCardB664 != NULL, 0xbc);
+    work = sSflCardB664;
+    work[0x4a1f] = 0;
+    *work |= 0x40;
+    work[0x4a1c] = 3;
+    *work |= 0x100;
+}
+
+// FUN_002592c0
+u32 sflCard002592c0(void)
+{
+    K_ASSERT(sSflCardB664 != NULL, 0xbc);
+    return *sSflCardB664 & 0x100;
+}
+
+// FUN_00259310
+void sflCard00259310(void)
+{
+    u32* work;
+
+    K_ASSERT(sSflCardB664 != NULL, 0xbc);
+    work = sSflCardB664;
+    work[0x4a1f] = 0;
+    *work |= 0x40;
+    work[0x4a1c] = 4;
+    *work |= 0x800;
+}
+
+// FUN_00259380
+u32 sflCard00259380(void)
+{
+    K_ASSERT(sSflCardB664 != NULL, 0xbc);
+    return *sSflCardB664 & 0x800;
+}
+
+// FUN_00258490
+void sflCard00258490(void)
+{
+    K_ASSERT(sSflCard354 != NULL, 0xbc);
+    *sSflCard354 |= 8;
+}
+
+// FUN_002584e0
+void sflCard002584e0(u32 param_1)
+{
+    K_ASSERT(sSflCard354 != NULL, 0xbc);
+    sSflCard354[0x49ec] = param_1;
 }
