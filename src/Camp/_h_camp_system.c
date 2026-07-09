ed6832cf07422ccd6fb062d4bc1f2256c7796b29
Raikaru <raikarualt001@gmail.com>
2026-07-09 03:35:20 -0400

    _h_camp_system.c: add SystemMenu destroy

--- src/Camp/_h_camp_system.c
+++ src/Camp/_h_camp_system.c
@@ -1,3 +1,45 @@
 #include "Camp/_h_camp_system.h"
+#include "Kernel/Kwln/kwlnTask.h"
+#include "rw/rwplcore.h"
 
-// TODO
+void FUN_004d0f00();
+void FUN_001124b0();
+void FUN_00100ec0();
+void FUN_003c7dd0();
+
+// FUN_00154770
+void h_campSystem00154770(int param_1)
+{
+    int* workData;
+    int i;
+
+    workData = *(int**)(param_1 + 0x3c);
+    if (workData[8] != 0) {
+        FUN_004d0f00(workData[8]);
+    }
+    if (workData[5] != 0) {
+        FUN_001124b0(workData[5]);
+    }
+    workData[5] = 0;
+    if (workData[6] != 0) {
+        FUN_001124b0(workData[6]);
+    }
+    workData[6] = 0;
+    if (workData[3] != 0) {
+        RwFree((void*)workData[3]);
+    }
+    workData[3] = 0;
+    if (workData[4] != 0) {
+        RwFree((void*)workData[4]);
+    }
+    workData[4] = 0;
+    FUN_003c7dd0(8);
+    FUN_003c7dd0(0xc);
+    for (i = 0; i < 3; i++) {
+        if (*(int*)((int)workData + i * 4 + 0x24) != 0) {
+            FUN_00100ec0(*(int*)((int)workData + i * 4 + 0x24));
+        }
+        *(int*)((int)workData + i * 4 + 0x24) = 0;
+    }
+    RwFree(workData);
+}
