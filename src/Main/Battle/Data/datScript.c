dc49b026b2854fd6ac6c197e1999a2ebfef4b9ed
Raikaru <raikarualt001@gmail.com>
2026-07-09 02:24:30 -0400

    datScript +predicate getter; add fclShopMisc.c memcpy setter

--- src/Main/Battle/Data/datScript.c
+++ src/Main/Battle/Data/datScript.c
@@ -1,4 +1,5 @@
 #include "Main/Battle/Data/datScript.h"
+#include "Kosaka/k_assert.h"
 
 static ScrData* sUnk_007ce514; // doesn't looks like it's used
 
@@ -6,4 +7,13 @@ static ScrData* sUnk_007ce514; // doesn't looks like it's used
 u8 datScript003112c0(ScrData* scr)
 {
     return sUnk_007ce514 == scr;
+}
+
+extern u16* DAT_00957190[]; // 0x957190
+
+// FUN_003111f0
+u32 datScript003111f0(void)
+{
+    K_ASSERT(DAT_00957190[0] != NULL, 0x205);
+    return !(*DAT_00957190[0] & 4);
 }
