79850ed2eb95fe2043c357753f0fe50db660bb13
Raikaru <raikarualt001@gmail.com>
2026-07-20 03:31:40 -0400

    Improve social cursor update and renderer decompilation

--- src/Main/Social/sfl_cursor.c
+++ src/Main/Social/sfl_cursor.c
@@ -12,6 +12,12 @@ void FUN_0025aad0(void);
 void FUN_0025b440(void);
 extern code DAT_00960090;
 extern code DAT_0096009c;
+#pragma alias DAT_00960090_abs DAT_00960090
+#pragma alias DAT_0096009c_abs DAT_0096009c
+extern code DAT_00960090_abs[];
+extern code DAT_0096009c_abs[];
+#define DAT_00960090 DAT_00960090_abs
+#define DAT_0096009c DAT_0096009c_abs
 extern f32 fGpffff8088;
 extern f32 fGpffff8248;
 extern f32 fGpffff82ac;
@@ -101,311 +107,164 @@ void FUN_0025a120(void)
 
 
 void FUN_0025a130(void)
-
-
-
 {
-
   u32 *puVar1;
+  u32 *puVar2;
+  s32 uVar3;
+  f32 fVar4;
+  f32 fStack_10;
+  f32 fStack_14;
+  f32 fStack_18;
 
-  u32 uVar2;
-
-  float fVar3;
-
-  
-
-  if (sSflCursor == (u32 *)0x0) {
-
-    FUN_0019d3f0(0x68ebb8,0x47);
-
-  }
-
-  if ((~*sSflCursor & 1) == 0) {
-
-    uVar2 = sSflCursor[0xd];
-
-    sSflCursor[0xd] = uVar2 + 1;
-
-    sSflCursor[0xd] = (int)(uVar2 + 1) % 0x3c;
-
-    if ((*sSflCursor & 2) != 0) {
-
-      uVar2 = sSflCursor[0x328];
-
-      if (uVar2 == 2) {
-
-        uVar2 = sSflCursor[10];
-
-        sSflCursor[10] = uVar2 + 1;
-
-        if (uVar2 + 1 == 0x10) {
-
-          *sSflCursor = *sSflCursor & 0xfffffffd;
-
+  K_ASSERT(sSflCursor != NULL, 0x47);
+  puVar1 = sSflCursor;
+  if ((~*puVar1 & 1) == 0) {
+    uVar3 = puVar1[0xd];
+    puVar1[0xd] = uVar3 + 1;
+    puVar1[0xd] = (uVar3 + 1) % 0x3c;
+    if ((*puVar1 & 2) != 0) {
+      uVar3 = puVar1[0x328];
+      if (uVar3 == 2) {
+        uVar3 = puVar1[10];
+        puVar1[10] = uVar3 + 1;
+        if (uVar3 + 1 == 0x10) {
+          *puVar1 = *puVar1 & 0xfffffffd;
         }
-
-      }
-
-      else if (uVar2 == 3) {
-
-        uVar2 = sSflCursor[10];
-
-        sSflCursor[10] = uVar2 + 1;
-
-        if (uVar2 + 1 == 0x10) {
-
-          uVar2 = *sSflCursor;
-
-          *sSflCursor = uVar2 & 0xfffffffd;
-
-          *sSflCursor = uVar2 & 0xfffffffc;
-
+      } else if (uVar3 == 3) {
+        uVar3 = puVar1[10];
+        puVar1[10] = uVar3 + 1;
+        if (uVar3 + 1 == 0x10) {
+          uVar3 = *puVar1;
+          *puVar1 = uVar3 & 0xfffffffd;
+          *puVar1 = uVar3 & 0xfffffffc;
         }
-
-      }
-
-      else if (uVar2 == 1) {
-
-        uVar2 = sSflCursor[10];
-
-        sSflCursor[10] = uVar2 + 1;
-
-        if (uVar2 + 1 == 4) {
-
-          *sSflCursor = *sSflCursor & 0xfffffffd;
-
+      } else if (uVar3 == 1) {
+        uVar3 = puVar1[10];
+        puVar1[10] = uVar3 + 1;
+        if (uVar3 + 1 == 4) {
+          *puVar1 = *puVar1 & 0xfffffffd;
         }
-
-      }
-
-      else if (uVar2 == 0) {
-
-        uVar2 = sSflCursor[10];
-
-        sSflCursor[10] = uVar2 + 1;
-
-        if (uVar2 + 1 == 4) {
-
-          *sSflCursor = *sSflCursor & 0xfffffffd;
-
+      } else if (uVar3 == 0) {
+        uVar3 = puVar1[10];
+        puVar1[10] = uVar3 + 1;
+        if (uVar3 + 1 == 4) {
+          *puVar1 = *puVar1 & 0xfffffffd;
         }
-
-        fVar3 = (float)(int)sSflCursor[10] / 4.0;
-
-        sSflCursor[7] =
-
-             (u32)(((float)sSflCursor[4] - (float)sSflCursor[1]) * fVar3 +
-
-                   (float)sSflCursor[1]);
-
-        sSflCursor[8] =
-
-             (u32)(((float)sSflCursor[5] - (float)sSflCursor[2]) * fVar3 +
-
-                   (float)sSflCursor[2]);
-
-        sSflCursor[9] =
-
-             (u32)(((float)sSflCursor[6] - (float)sSflCursor[3]) * fVar3 +
-
-                   (float)sSflCursor[3]);
-
+        fVar4 = (f32)(s32)puVar1[10] / 4.0f;
+        fStack_10 = (f32)puVar1[4] - (f32)puVar1[1];
+        fStack_14 = (f32)puVar1[5] - (f32)puVar1[2];
+        fStack_18 = (f32)puVar1[6] - (f32)puVar1[3];
+        fStack_10 *= fVar4;
+        fStack_14 *= fVar4;
+        fStack_18 *= fVar4;
+        fStack_10 += (f32)puVar1[1];
+        fStack_14 += (f32)puVar1[2];
+        fStack_18 += (f32)puVar1[3];
+        puVar1[7] = (u32)fStack_10;
+        puVar1[8] = (u32)fStack_14;
+        puVar1[9] = (u32)fStack_18;
       }
-
     }
-
-    for (uVar2 = 0; (int)uVar2 < 6; uVar2 = uVar2 + 1) {
-
-      puVar1 = sSflCursor + uVar2 * 0x84 + 0x10;
-
-      if (uVar2 == sSflCursor[0xc]) {
-
-        if ((int)*puVar1 < 0x100) {
-
-          *puVar1 = *puVar1 + 0x19;
-
+    for (uVar3 = 0; (s32)uVar3 < 6; uVar3 = uVar3 + 1) {
+      puVar2 = puVar1 + uVar3 * 0x84 + 0x10;
+      if (uVar3 == puVar1[0xc]) {
+        if ((s32)*puVar2 < 0x100) {
+          *puVar2 = *puVar2 + 0x19;
         }
-
-        if (0x100 < (int)*puVar1) {
-
-          *puVar1 = 0x100;
-
+        if (0x100 < (s32)*puVar2) {
+          *puVar2 = 0x100;
         }
-
+      } else {
+        if (*puVar2 != 0) {
+          *puVar2 = *puVar2 - 8;
+        }
+        if ((s32)*puVar2 < 0) {
+          *puVar2 = 0;
+        }
       }
-
-      else {
-
-        if (*puVar1 != 0) {
-
-          *puVar1 = *puVar1 - 8;
-
+      puVar2++;
+      if (uVar3 == puVar1[0xc]) {
+        if ((s32)*puVar2 < 0x100) {
+          *puVar2 = *puVar2 + 0x40;
         }
-
-        if ((int)*puVar1 < 0) {
-
-          *puVar1 = 0;
-
+        if (0x100 < (s32)*puVar2) {
+          *puVar2 = 0x100;
         }
-
+      } else {
+        if (*puVar2 != 0) {
+          *puVar2 = *puVar2 - 0xc;
+        }
+        if ((s32)*puVar2 < 0) {
+          *puVar2 = 0;
+        }
       }
-
-      if (uVar2 == sSflCursor[0xc]) {
-
-        if ((int)sSflCursor[uVar2 * 0x84 + 0x11] < 0x100) {
-
-          sSflCursor[uVar2 * 0x84 + 0x11] = sSflCursor[uVar2 * 0x84 + 0x11] + 0x40;
-
-        }
-
-        if (0x100 < (int)sSflCursor[uVar2 * 0x84 + 0x11]) {
-
-          sSflCursor[uVar2 * 0x84 + 0x11] = 0x100;
-
-        }
-
-      }
-
-      else {
-
-        if (sSflCursor[uVar2 * 0x84 + 0x11] != 0) {
-
-          sSflCursor[uVar2 * 0x84 + 0x11] = sSflCursor[uVar2 * 0x84 + 0x11] - 0xc;
-
-        }
-
-        if ((int)sSflCursor[uVar2 * 0x84 + 0x11] < 0) {
-
-          sSflCursor[uVar2 * 0x84 + 0x11] = 0;
-
-        }
-
-      }
-
     }
-
-
   }
-
   return;
-
 }
 
 // FUN_0025A440 NONMATCHING
 
 
 void FUN_0025a440(void)
-
-
-
 {
-
+  volatile code *pRender;
+  volatile code *pTex;
   u32 *puVar1;
+  u32 *puVar2;
+  u32 uVar2;
+  s32 bVar3;
+  s32 uVar4;
 
-  u64 uVar2;
-
-  u8 bVar3;
-
-  u32 uVar4;
-
-  
-
-  if (sSflCursor == (u32 *)0x0) {
-
-    FUN_0019d3f0(0x68ebb8,0x47);
-
-  }
-
+  K_ASSERT(sSflCursor != NULL, 0x47);
   puVar1 = sSflCursor;
-
-  if ((~*sSflCursor & 1) == 0) {
-
-    (*DAT_00960090)(9,2);
-
-    (*DAT_00960090)(0x14,2);
-
-    (*DAT_00960090)(8,0);
-
-    (*DAT_00960090)(6,0);
-
+  if ((~*puVar1 & 1) == 0) {
+    pRender = DAT_00960090_abs;
+    (*pRender)(9,2);
+    (*pRender)(0x14,2);
+    (*pRender)(8,0);
+    (*pRender)(6,0);
     FUN_004d7f60(3,0x717fb);
-
     FUN_004d7f60(2,0x44);
-
     uVar2 = FUN_0020e510(4);
-
-    (*DAT_00960090)(1,uVar2);
-
-    (*DAT_0096009c)(puVar1 + 0x32c,4,0,1,2);
-
-    (*DAT_0096009c)(puVar1 + 0x32c,4,0,2,3);
-
-    (*DAT_0096009c)(puVar1 + 0x36c,4,0,1,2);
-
-    (*DAT_0096009c)(puVar1 + 0x36c,4,0,2,3);
-
+    (*pRender)(1,uVar2);
+    pTex = DAT_0096009c_abs;
+    (*pTex)(puVar1 + 0x32c,4,0,1,2);
+    (*pTex)(puVar1 + 0x32c,4,0,2,3);
+    (*pTex)(puVar1 + 0x36c,4,0,1,2);
+    (*pTex)(puVar1 + 0x36c,4,0,2,3);
     uVar2 = FUN_0020e590(2);
-
-    (*DAT_00960090)(1,uVar2);
-
-    (*DAT_0096009c)(puVar1 + 0x3ac,4,0,1,2);
-
-    (*DAT_0096009c)(puVar1 + 0x3ac,4,0,2,3);
-
+    (*pRender)(1,uVar2);
+    (*pTex)(puVar1 + 0x3ac,4,0,1,2);
+    (*pTex)(puVar1 + 0x3ac,4,0,2,3);
     FUN_004d7f60(3,0x71801);
-
     FUN_004d7f60(2,0x48);
-
-    for (uVar4 = 0; (int)uVar4 < 6; uVar4 = uVar4 + 1) {
-
+    for (uVar4 = 0; uVar4 < 6; uVar4 = uVar4 + 1) {
+      puVar2 = puVar1 + uVar4 * 0x84 + 0x10;
       bVar3 = 0;
-
       if (((*puVar1 & 2) == 0) || (puVar1[0x328] != 1)) {
-
-        if (uVar4 == puVar1[0xc]) {
-
+        if (uVar4 == (s32)puVar1[0xc]) {
           bVar3 = 1;
-
         }
-
-      }
-
-      else if ((uVar4 == puVar1[0xc]) || (uVar4 == puVar1[0xb])) {
-
+      } else if ((uVar4 == (s32)puVar1[0xc]) ||
+                 (uVar4 == (s32)puVar1[0xb])) {
         bVar3 = 1;
-
       }
-
       if (bVar3) {
-
         uVar2 = FUN_0020e510(2);
-
-        (*DAT_00960090)(1,uVar2);
-
-        (*DAT_0096009c)(puVar1 + uVar4 * 0x84 + 0x14,4,0,1,2);
-
-        (*DAT_0096009c)(puVar1 + uVar4 * 0x84 + 0x14,4,0,2,3);
-
+        (*pRender)(1,uVar2);
+        (*pTex)(puVar2 + 4,4,0,1,2);
+        (*pTex)(puVar2 + 4,4,0,2,3);
       }
-
-      if (puVar1[uVar4 * 0x84 + 0x10] != 0) {
-
+      if (*puVar2 != 0) {
         uVar2 = FUN_0020e510(1);
-
-        (*DAT_00960090)(1,uVar2);
-
-        (*DAT_0096009c)(puVar1 + uVar4 * 0x84 + 0x54,4,0,1,2);
-
-        (*DAT_0096009c)(puVar1 + uVar4 * 0x84 + 0x54,4,0,2,3);
-
+        (*pRender)(1,uVar2);
+        (*pTex)(puVar2 + 0x44,4,0,1,2);
+        (*pTex)(puVar2 + 0x44,4,0,2,3);
       }
-
     }
-
   }
-
   return;
-
 }
 
 // FUN_0025A7D0
@@ -554,11 +413,7 @@ void FUN_0025aad0(void)
 
   
 
-  if (sSflCursor == (u32 *)0x0) {
-
-    FUN_0019d3f0(0x68ebb8,0x47);
-
-  }
+  K_ASSERT(sSflCursor != NULL, 0x47);
 
   puVar1 = sSflCursor;
 
