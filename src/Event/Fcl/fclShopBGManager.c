5870c62b00c2b56639618b4eb673ffedb0e4dd8d
Raikaru <raikarualt001@gmail.com>
2026-07-22 15:38:51 -0400

    Improve ShopBG text draw call typing

--- src/Event/Fcl/fclShopBGManager.c
+++ src/Event/Fcl/fclShopBGManager.c
@@ -24,6 +24,8 @@ void FUN_0040e3c0(float, s32, s32, u8, s32, s32);
 void FUN_0040e3c0_i(s32, s32, float, u8, s32, s32);
 #pragma alias FUN_0040e3f0_typed FUN_0040e3f0
 void FUN_0040e3f0_typed(float, float, float, float, s32, s32, u8, s32, s32, s32, s32);
+#pragma alias FUN_003b2cb0_typed FUN_003b2cb0
+void FUN_003b2cb0_typed(float, s32, s32, u32, u32, u32, u32, u32, u32);
 void FUN_003eec60(s32);
 u64 FUN_004101b0(u64 param_1,int param_2);
 u32 FUN_004105e0(int param_1,int param_2,long param_3);
@@ -4132,13 +4134,13 @@ void FUN_00414770(u64 param_1,int param_2,int param_3,int param_4,int param_5,u3
 
   FUN_0040e3c0((float)param_5,param_3 + 0x139,param_4 + 0x27,param_6 & 0xff,0x8c,0);
 
-  FUN_0040e3c0(0,param_3 + 0x16a,param_4 + 0x2a,param_6 & 0xff,0,
+  FUN_0040e3c0(0.0f,param_3 + 0x16a,param_4 + 0x2a,param_6 & 0xff,0,
 
                (u32)puVar1[0x7c] * 2 + 1);
 
   uVar2 = func_00171110(*(s16 *)(puVar1 + 0x78),2);
 
-  FUN_003b2cb0(0,param_3 + 0x18a,param_4 + 0x2c,param_6 | 0xffffffffffffff00,6,1,uVar2,0,0);
+  FUN_003b2cb0_typed(0,param_3 + 0x18a,param_4 + 0x2c,param_6 | 0xffffff00,6,1,uVar2,0,0);
 
   return;
 
