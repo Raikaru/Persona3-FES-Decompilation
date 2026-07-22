a9057f28f58151f87ef76b62169b5a88114feafe
Raikaru <raikarualt001@gmail.com>
2026-07-22 08:33:54 -0400

    Match FUN_003851f0 event menu renderer

--- src/Event/mt_evtMenu.c
+++ src/Event/mt_evtMenu.c
@@ -171,6 +171,8 @@ extern u8 DAT_006a0080_abs[];
 extern u8 DAT_006a01e0_abs[];
 #pragma alias DAT_006a01a0_abs DAT_006a01a0
 extern u8 DAT_006a01a0_abs[];
+#pragma alias DAT_006a0230_abs DAT_006a0230
+extern u8 DAT_006a0230_abs[];
 extern u8 DAT_006a0350_abs[];
 extern u8 DAT_006a03f0_abs[];
 extern u8 DAT_006a0530_abs[];
@@ -18679,7 +18681,7 @@ u32 FUN_003851f0(int param_1,int param_2,int param_3)
 
   uVar1 = FUN_0038a220(*(u32 *)(param_3 + 0xe0));
 
-  FUN_0038a260(uVar1,param_1 * 0xc,param_2 * 0xc,0,0x6a0230);
+  FUN_0038a260_f32_5(uVar1,param_1 * 0xc,param_2 * 0xc,0,DAT_006a0230_abs);
 
   return 2;
 
