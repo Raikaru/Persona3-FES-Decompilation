121c3958d1d4a4c0a3f1f4ccfcc2696fc274ae79
Raikaru <raikarualt001@gmail.com>
2026-07-22 08:30:00 -0400

    Match FUN_003756e0 menu effect pointer

--- src/Event/mt_evtMenu.c
+++ src/Event/mt_evtMenu.c
@@ -6214,7 +6214,7 @@ u32 FUN_003756e0(int param_1,int param_2,int param_3)
 
   uVar1 = FUN_0038a220(*(u32 *)(param_3 + 0xe0));
 
-  FUN_0038a260(uVar1,param_1 * 0xc,param_2 * 0xc,0,0x69ec38);
+  FUN_0038a260_f32_5(uVar1,param_1 * 0xc,param_2 * 0xc,0,DAT_0069ec38);
 
   return 2;
 
