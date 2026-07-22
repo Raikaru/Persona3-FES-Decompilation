a072b9b7a5fca8456641700d21d11e110377448c
Raikaru <raikarualt001@gmail.com>
2026-07-22 06:57:09 -0400

    Improve mt_evtMenu FUN_00375a40 match

--- src/Event/mt_evtMenu.c
+++ src/Event/mt_evtMenu.c
@@ -1723,12 +1723,14 @@ u32 FUN_00370230(int param_1,int param_2,int param_3)
 {
 
   f32 uVar1;
+  u32 uVar2;
 
   
 
   uVar1 = FUN_0038a220(*(u32 *)(param_3 + 0xe0));
 
-  FUN_0038a260_f32_5(uVar1,param_1 * 0xc,param_2 * 0xc,0,(void *)0x69e6f0);
+  uVar2 = 0x6a000000;
+  FUN_0038a260_f32_5(uVar1,param_1 * 0xc,param_2 * 0xc,0,(void *)(uVar2 - 0x1910));
 
   return 2;
 
@@ -6381,7 +6383,8 @@ u32 FUN_00375a40(int param_1,int param_2,int param_3)
 
   uVar1 = FUN_0038a220(*(u32 *)(param_3 + 0xe0));
 
-  FUN_0038a260(uVar1,param_1 * 0xc,param_2 * 0xc,0,0x69ecb8);
+  FUN_0038a260_f32_5(uVar1,param_1 * 0xc,param_2 * 0xc,0,
+                     (void *)((u8 *)0x6a000000 - 0x1348));
 
   return 2;
 
