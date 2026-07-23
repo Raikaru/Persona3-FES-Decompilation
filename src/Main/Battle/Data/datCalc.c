a7a4e8494ed3feea2193aa97303b2f83f29cfe68
Raikaru <raikarualt001@gmail.com>
2026-07-22 21:52:53 -0400

    Match datCalc FUN_0030e030 float operand order

--- src/Main/Battle/Data/datCalc.c
+++ src/Main/Battle/Data/datCalc.c
@@ -6880,6 +6880,7 @@ void FUN_0030cf00(void)
   FUN_00311190((int)(fVar3 + 0.0f));
   return;
 }
+#pragma schedule on
 
 
 
@@ -7308,28 +7309,31 @@ void FUN_0030e030(void)
   s32 lVar1;
   float fVar2;
   float fVar3;
+  float fVar4;
 
   lVar1 = FUN_003111f0();
   if (lVar1 == 0)
     goto zero;
   fVar2 = (float)FUN_0030fdf0(0xd,4);
   fVar3 = 50.0f;
-  fVar2 = fVar3 + fVar2;
-  fVar3 = (float)FUN_0030fdf0(0xe,4);
-  fVar3 = fVar3 + 50.0f;
-  fVar2 = fVar2 / fVar3;
+  fVar4 = fVar3 + fVar2;
+  fVar2 = (float)FUN_0030fdf0(0xe,4);
+  fVar3 = 50.0f;
+  fVar3 = fVar3 + fVar2;
+  fVar4 = fVar4 / fVar3;
   fVar3 = (float)FUN_0030fdf0(0x14,0);
-  fVar3 = fVar3 * fVar2;
+  fVar3 = fVar3 * fVar4;
   goto done;
 zero:
   fVar2 = (float)FUN_0030fdf0(0xd,4);
   fVar3 = 50.0f;
-  fVar2 = fVar3 + fVar2;
-  fVar3 = (float)FUN_0030fdf0(0xe,4);
-  fVar3 = fVar3 + 50.0f;
-  fVar2 = fVar2 / fVar3;
+  fVar4 = fVar3 + fVar2;
+  fVar2 = (float)FUN_0030fdf0(0xe,4);
+  fVar3 = 50.0f;
+  fVar3 = fVar3 + fVar2;
+  fVar4 = fVar4 / fVar3;
   fVar3 = (float)FUN_0030fdf0(0x14,0);
-  fVar3 = fVar3 * fVar2 * 0.5f;
+  fVar3 = fVar3 * fVar4 * 0.5f;
 done:
   FUN_00311190((int)fVar3);
 }
