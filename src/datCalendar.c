6aa42b0f70ae610b8dc05fec5b90456d89bbf527
Raikaru <raikarualt001@gmail.com>
2026-07-22 21:47:05 -0400

    Improve calendar alpha draw scheduling

--- src/datCalendar.c
+++ src/datCalendar.c
@@ -4100,8 +4100,6 @@ void func_00185b40(void* resource,
 // FUN_00186050 NONMATCHING
 void func_00186050(void* resource, u64 position, u32 alpha)
 {
-    f32 y;
-    void* unused;
     union
     {
         u64 value;
@@ -4111,14 +4109,19 @@ void func_00186050(void* resource, u64 position, u32 alpha)
             f32 y;
         } coords;
     } packed;
+    f32 y;
+    f32 x;
+    void* unused;
 
     packed.value = position;
     y = packed.coords.y;
     asm volatile("" : "+m"(y));
     func_001159f0(unused, resource, 0x2b, alpha & 0xff,
                   packed.coords.x, y, 48.0f);
+    x = packed.coords.x + 449.0f;
+    asm volatile("" : "+m"(x));
     func_001159f0(unused, resource, 0x2c, alpha & 0xff,
-                  packed.coords.x + 449.0f, y, 48.0f);
+                  x, y, 48.0f);
 }
 #pragma pop
 
