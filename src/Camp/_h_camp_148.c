87cb0699dc4ad782c7c207cd9c3048287dc3cdaf
Raikaru <raikarualt001@gmail.com>
2026-07-23 22:33:45 -0400

    Complete Camp status overview transition arguments

--- src/Camp/_h_camp_148.c
+++ src/Camp/_h_camp_148.c
@@ -197,33 +197,33 @@ void h_campDrawStatusOverview(int param_1)
     pair.x = (f32)0x17b;
     pair.y = 12.0f;
     sp98.u = *(u64*)&pair;
-    sp98.f[0] = pair.x + 0.0f;
-    campDrawTransition(100.0f, (void*)(*(u32*)(param_1 + 0xc0)), 0, 2, 2, sp98.u, *(u64*)&pair, 0, 0, 0, 0);
+    sp98.f[0] += 0.0f;
+    func_0018bc10(100.0f, (void*)(*(u32*)(param_1 + 0xc0)), 0, 2, 2, sp98.u, *(u64*)&pair, 0, 0, 0, 10);
     pair.x = 13.0f;
     pair.y = 27.0f;
     sp40.u = *(u64*)&pair;
     sp40.f[0] += 0.0f;
-    campDrawTransition(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0x44), 0, 2, 2, sp40.u, *(u64*)&pair, 0, 0, 0, 0);
+    func_0018bc10(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0x44), 0, 2, 2, sp40.u, *(u64*)&pair, 0, 0, 0, 10);
     pair.x = 112.0f;
     pair.y = 27.0f;
     sp48.u = *(u64*)&pair;
     sp48.f[0] += 0.0f;
-    campDrawTransition(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0x88), 0, 2, 2, sp48.u, *(u64*)&pair, 0, 0, 0, 0);
+    func_0018bc10(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0x88), 0, 2, 2, sp48.u, *(u64*)&pair, 0, 0, 0, 10);
     pair.x = 135.0f;
     pair.y = 27.0f;
     sp50.u = *(u64*)&pair;
     sp50.f[0] += 0.0f;
-    campDrawTransition(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0xcc), 0, 2, 2, sp50.u, *(u64*)&pair, 0, 0, 0, 0);
+    func_0018bc10(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0xcc), 0, 2, 2, sp50.u, *(u64*)&pair, 0, 0, 0, 10);
     pair.x = 278.0f;
     pair.y = 27.0f;
     sp58.u = *(u64*)&pair;
     sp58.f[0] += 0.0f;
-    campDrawTransition(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0x110), 0, 2, 2, sp58.u, *(u64*)&pair, 0, 0, 0, 0);
+    func_0018bc10(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0x110), 0, 2, 2, sp58.u, *(u64*)&pair, 0, 0, 0, 10);
     pair.x = (f32)0x12d;
     pair.y = 27.0f;
     sp60.u = *(u64*)&pair;
     sp60.f[0] += 0.0f;
-    campDrawTransition(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0x154), 0, 2, 2, sp60.u, *(u64*)&pair, 0, 0, 0, 0);
+    func_0018bc10(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0x154), 0, 2, 2, sp60.u, *(u64*)&pair, 0, 0, 0, 10);
     for (i = 0; i < 4; i++) {
         if (*(s32*)(param_1 + 0x1c) - 1 < i)
             *(u32*)(*(u32*)(param_1 + 0xc0) + i * 0x44 + 0x2ac) = 0;
@@ -232,29 +232,29 @@ void h_campDrawStatusOverview(int param_1)
             pair.y = 64.0f + (f32)(i * 0x55);
             sp68.u = *(u64*)&pair;
             sp68.f[0] += 0.0f;
-            campDrawTransition(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + (i + 10) * 0x44), 0, 2, 2, sp68.u, *(u64*)&pair, 0, 0, 0, 0);
+            func_0018bc10(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + (i + 10) * 0x44), 0, 2, 2, sp68.u, *(u64*)&pair, 0, 0, 0, 10);
         }
     }
     pair.x = 104.0f;
     pair.y = 61.0f;
     sp70.u = *(u64*)&pair;
     sp70.f[0] += 0.0f;
-    campDrawTransition(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0x550), 0, 2, 2, sp70.u, *(u64*)&pair, 0, 0, 0, 0);
+    func_0018bc10(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0x550), 0, 2, 2, sp70.u, *(u64*)&pair, 0, 0, 0, 10);
     pair.x = 124.0f;
     pair.y = 219.0f;
     sp78.u = *(u64*)&pair;
     sp78.f[0] += 0.0f;
-    campDrawTransition(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0x594), 0, 2, 2, sp78.u, *(u64*)&pair, 0, 0, 0, 0);
+    func_0018bc10(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0x594), 0, 2, 2, sp78.u, *(u64*)&pair, 0, 0, 0, 10);
     pair.x = 226.0f;
     pair.y = (f32)0x19f;
     sp80.u = *(u64*)&pair;
     sp80.f[0] += 0.0f;
-    campDrawTransition(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0x83c), 0, 2, 2, sp80.u, *(u64*)&pair, 0, 0, 0, 0);
+    func_0018bc10(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0x83c), 0, 2, 2, sp80.u, *(u64*)&pair, 0, 0, 0, 10);
     pair.x = (f32)0x1b7;
     pair.y = (f32)0x19f;
     sp88.u = *(u64*)&pair;
     sp88.f[0] += 0.0f;
-    campDrawTransition(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0x880), 0, 2, 2, sp88.u, *(u64*)&pair, 0, 0, 0, 0);
+    func_0018bc10(100.0f, (void*)(*(u32*)(param_1 + 0xc0) + 0x880), 0, 2, 2, sp88.u, *(u64*)&pair, 0, 0, 0, 10);
 }
 // FUN_00148880 NONMATCHING
 void h_campDrawListEntry(int param_1,int param_2,int param_3)
