af394ac9855253fe2887a7605ed6eea7450c053e
Raikaru <raikarualt001@gmail.com>
2026-07-26 13:50:08 -0400

    Improve itfPanel near-match codegen

--- src/itfPanel.c
+++ src/itfPanel.c
@@ -4336,9 +4336,9 @@ u64 FUN_003ada00(u64 param_1,int param_2)
 
   char msg[8];
 
-  u8 bVar1;
-
   int bVar7;
+
+  u8 uVar2;
 
   u64 uVar3;
 
@@ -4346,9 +4346,9 @@ u64 FUN_003ada00(u64 param_1,int param_2)
 
   int iVar5;
 
-  u8 uVar2;
-
   u8 bVar6;
+
+  u8 bVar1;
   int offset;
   int base;
 
