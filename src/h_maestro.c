05bf1bc348e3440d3db31290752dbe4ce0698c13
Raikaru <raikarualt001@gmail.com>
2026-07-25 20:58:08 -0400

    Reconstruct func_00114e70

--- src/h_maestro.c
+++ src/h_maestro.c
@@ -2408,6 +2408,7 @@ void func_00114af0(f32 depth,
     (*D_009600A0)(rwPRIMTYPETRISTRIP, vertices, 4);
 }
 
+// Retail reconstruction: camera/state setup (0x114e70-0x114fec), vertex/color loop (0x115030-0x115164), UV/orientation staging (0x115168-0x115294), and final draw (0x1152a0-0x115300).
 // FUN_00114E70 NONMATCHING
 void func_00114e70(f32 depth,
                    f32 x,
@@ -2421,53 +2422,103 @@ void func_00114e70(f32 depth,
                    const s32* textureDimensions)
 {
     RwIm2DVertex vertices[4];
-    f32 u0;
-    f32 v0;
-    f32 u1;
-    f32 v1;
+    f32 corners[4][2];
+    f32 textureCoordinates[4][2];
+    u64 swap;
+    f32 farX;
+    f32 farY;
     f32 recipZ;
+    f32 z;
+    void (**setState)(u32, u32);
+    RwCamera* camera;
+    s32 r;
+    s32 g;
+    s32 b;
+    s32 a;
+    s32 i;
 
-    Maestro_SetPrimitiveStates(0x44, 0x717fb);
-    if (textureDimensions != NULL && textureDimensions[3] != 0 && textureDimensions[4] != 0)
+    camera = kwlnGetMainCamera();
+    recipZ = 1.0f / camera->nearPlane;
+
+    r = (s32)((color & 0xff000000) >> 24);
+    g = (s32)((color & 0xff0000) >> 16);
+    b = (s32)((color & 0xff00) >> 8);
+    a = (s32)(color & 0xff);
+
+    setState = (void (**)(u32, u32))D_00960090_abs;
+    (*setState)(6, 1);
+    (*setState)(7, 2);
+    (*setState)(8, 1);
+    (*setState)(9, 2);
+    (*setState)(0xc, 1);
+    (*setState)(0xb, 6);
+    (*setState)(0xa, 5);
+    (*setState)(2, 4);
+    RpSkyRenderStateSet(2, (void*)0x44);
+    RpSkyRenderStateSet(3, (void*)0x717fb);
+
+    corners[0][0] = x;
+    corners[0][1] = y;
+    farX = x + (f32)width;
+    corners[3][0] = farX;
+    farY = y + (f32)height;
+    corners[3][1] = farY;
+    corners[1][0] = farX;
+    corners[1][1] = y;
+    corners[2][0] = x;
+    corners[2][1] = farY;
+    z = D_00960088 - depth;
+
+    for (i = 0; i < 4; i++)
     {
-        u0 = textureX / (f32)textureDimensions[3];
-        v0 = textureY / (f32)textureDimensions[4];
-        u1 = (textureX + (f32)width) / (f32)textureDimensions[3];
-        v1 = (textureY + (f32)height) / (f32)textureDimensions[4];
+        RwIm2DVertex* v = &vertices[i];
+        v->u.els.scrVertex.z = z;
+        v->u.els.recipZ = recipZ;
+        v->u.els.color.r = (f32)(u32)r;
+        v->u.els.color.g = (f32)(u32)g;
+        v->u.els.color.b = (f32)(u32)b;
+        v->u.els.color.a = (f32)(u32)a;
+        v->u.els.scrVertex.x = corners[i][0];
+        v->u.els.scrVertex.y = corners[i][1];
     }
-    else
-    {
-        u0 = 0.0f;
-        v0 = 0.0f;
-        u1 = 1.0f;
-        v1 = 1.0f;
-    }
+
+    textureCoordinates[0][0] = textureX / (f32)textureDimensions[3];
+    textureCoordinates[0][1] = textureY / (f32)textureDimensions[4];
+    textureCoordinates[3][0] = (textureX + (f32)width) / (f32)textureDimensions[3];
+    textureCoordinates[3][1] = (textureY + (f32)height) / (f32)textureDimensions[4];
+    textureCoordinates[1][0] = textureCoordinates[3][0];
+    textureCoordinates[1][1] = textureCoordinates[0][1];
+    textureCoordinates[2][0] = textureCoordinates[0][0];
+    textureCoordinates[2][1] = textureCoordinates[3][1];
+
     if (orientation == 2)
     {
-        f32 swap = u0;
-        u0 = u1;
-        u1 = swap;
+        swap = *(u64*)&textureCoordinates[0][0];
+        *(u64*)&textureCoordinates[0][0] = *(u64*)&textureCoordinates[2][0];
+        *(u64*)&textureCoordinates[2][0] = swap;
+        swap = *(u64*)&textureCoordinates[1][0];
+        *(u64*)&textureCoordinates[1][0] = *(u64*)&textureCoordinates[3][0];
+        *(u64*)&textureCoordinates[3][0] = swap;
     }
-    else if (orientation == 1)
+
+    if (orientation == 1)
     {
-        f32 swap = v0;
-        v0 = v1;
-        v1 = swap;
+        swap = *(u64*)&textureCoordinates[0][0];
+        *(u64*)&textureCoordinates[0][0] = *(u64*)&textureCoordinates[1][0];
+        *(u64*)&textureCoordinates[1][0] = swap;
+        swap = *(u64*)&textureCoordinates[2][0];
+        *(u64*)&textureCoordinates[2][0] = *(u64*)&textureCoordinates[3][0];
+        *(u64*)&textureCoordinates[3][0] = swap;
     }
-    (*D_00960090)(1, 0);
-    recipZ = Maestro_NearReciprocal();
-    Maestro_DrawQuad(&vertices[0],
-                     x,
-                     y,
-                     (f32)width,
-                     (f32)height,
-                     D_00960088 - depth,
-                     recipZ,
-                     color,
-                     u0,
-                     v0,
-                     u1,
-                     v1);
+
+    for (i = 0; i < 4; i++)
+    {
+        vertices[i].u.els.u = textureCoordinates[i][0];
+        vertices[i].u.els.v = textureCoordinates[i][1];
+    }
+
+    (*setState)(1, 0);
+    (*D_009600A0)(rwPRIMTYPETRISTRIP, vertices, 4);
 }
 
 // FUN_00115350 NONMATCHING
