450b63e204760c3a938f3295482e33ad284c2e66
Raikaru <raikarualt001@gmail.com>
2026-07-26 22:15:48 -0400

    Trim btlFormation func_002bf3f0 parameter

--- src/Yajima/y_runtime.c
+++ src/Yajima/y_runtime.c
@@ -3552,7 +3552,7 @@ void FUN_004310e0(int param_1)
 u32 FUN_00431110(u64 param_1,YajimaVec2 param_2,char param_3,u8 param_4)
 
 {
-  u8 uVar3;
+  int uVar3;
   u8 *puVar4;
   u32 lVar1;
   u32 uVar2;
