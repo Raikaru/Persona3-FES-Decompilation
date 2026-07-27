10690fb1af1b21c1048e2afba7d53ba5761ce691
Raikaru <raikarualt001@gmail.com>
2026-07-26 22:28:38 -0400

    Complete geometry error paths

--- src/Kosaka/Field/k_fieldRuntime.c
+++ src/Kosaka/Field/k_fieldRuntime.c
@@ -4494,6 +4494,10 @@ void func_001e9af0(RuntimeWork* workData, u32* renderRef)
         {
             func_0019d3f0(D_006844F8, 0x4a0);
         }
+        if (work->state == 2 && count < 2)
+        {
+            func_0019d3f0(D_006844F8, 0x4b1);
+        }
         (void)vector2;
         (void)vector3;
         (void)section;
