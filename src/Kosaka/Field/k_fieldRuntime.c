740979656d0a0af9a7b8a4f30612dd003af5ccfa
Raikaru <raikarualt001@gmail.com>
2026-07-26 22:29:00 -0400

    Add first geometry normal calculation

--- src/Kosaka/Field/k_fieldRuntime.c
+++ src/Kosaka/Field/k_fieldRuntime.c
@@ -4498,6 +4498,36 @@ void func_001e9af0(RuntimeWork* workData, u32* renderRef)
         {
             func_0019d3f0(D_006844F8, 0x4b1);
         }
+        if (work->state == 2 && vertices != NULL &&
+            vector2 != NULL && vector3 != NULL)
+        {
+            RuntimeVec3 edge0;
+            RuntimeVec3 edge1;
+            RuntimeVec3 normal0;
+            u32 geometryIndex;
+
+            geometryIndex = 0;
+            edge0.x = vector2[geometryIndex].x -
+                      vector3[geometryIndex].x;
+            edge0.y = vector2[geometryIndex].y -
+                      vector3[geometryIndex].y;
+            edge0.z = vector2[geometryIndex].z -
+                      vector3[geometryIndex].z;
+            edge1.x = vector0[geometryIndex].x -
+                      vector1[geometryIndex].x;
+            edge1.y = vector0[geometryIndex].y -
+                      vector1[geometryIndex].y;
+            edge1.z = vector0[geometryIndex].z -
+                      vector1[geometryIndex].z;
+            normal0.x = edge0.y * edge1.z - edge0.z * edge1.y;
+            normal0.y = edge0.z * edge1.x - edge0.x * edge1.z;
+            normal0.z = edge0.x * edge1.y - edge0.y * edge1.x;
+            func_004c69f0(&normal0, &normal0);
+            blended.x = vector2[geometryIndex].x + normal0.x;
+            blended.y = vector2[geometryIndex].y + normal0.y;
+            blended.z = vector2[geometryIndex].z + normal0.z;
+            vertices[geometryIndex] = blended;
+        }
         (void)vector2;
         (void)vector3;
         (void)section;
